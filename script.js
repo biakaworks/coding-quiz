bec265d7401a4a9fa0ee3c872061dcde92aff798
Biaka Z <biaka@biakaworks.com>
2021-02-05 01:52:41 -0600

    Updated a few more functions

--- script.js
+++ script.js
@@ -1,72 +1,93 @@
+// var timerLeft = 100;
+// var time;
+// var questionNumber = 0;
+var welcomeMessage = document.querySelector("#welcomeMessge");
+var startQuiz = document.querySelector("#startQuiz");
+var headerMain = document.querySelector("#headerMain");
 
-//1. do the cards play the game
-//2. add timer (stop count, wrongans, start)
-var timerLeft = 100;
-var time;
-var questionNumber = 0;
-//3. ls (score = time is left) & username
-//score all the ques, ans, choices
 var quizScore = 0;
-var indexQuestion=0;
+var indexQuestion = 0;
 var currentQuestion = questions[indexQuestion];
 
-var questions =[{
+var questions = [
+  {
     question: "Inside which HTML element do we put the JavaScript?",
     choices: ["js", "javascript", "scripting", "script"],
-    answer: "script"
-},
-{
+    answer: "script",
+  },
+  {
     question: "Where is the correct place to insert a JavaScript?",
-    choices: ["Both the <head> section and the <body> section are correct", "The <head> section", "The <body> section", "The <butt> section"],
-    answer: "Both the <head> section and the <body> section are correct"
-},
-{
-    question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choices: ["<script src='xxx.js'>  ", "<script name='xxx.js'>", "<script href='xxx.js'>", "None of the Above"],
-    answer: "<script src='xxx.js'>  "
-},
-{
+    choices: [
+      "Both the <head> section and the <body> section are correct",
+      "The <head> section",
+      "The <body> section",
+      "The <butt> section",
+    ],
+    answer: "Both the <head> section and the <body> section are correct",
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choices: [
+      "<script src='xxx.js'>  ",
+      "<script name='xxx.js'>",
+      "<script href='xxx.js'>",
+      "None of the Above",
+    ],
+    answer: "<script src='xxx.js'>  ",
+  },
+  {
     question: "Inside which HTML element do we put the JavaScript?",
     choices: ["js", "javascript", "scripting", "script"],
-    answer: "script"
-},
+    answer: "script",
+  },
+  {
+    question: "Which event occurs when the user clicks on an HTML element?",
+    choices: ["onclick ", "onmouseover", "onchange", "onmouseclick"],
+    answer: "onclick",
+  },
 ];
+function welcome() {
+    welcomeMessage.textContent = "Test your Javascript Skill! Click Beging Quiz!";}
+    renderWelcome()
 
-function renderCard(){
-    //clear the area blank canvas
-    //dynamically append the data to the page
-    console.log(game.question[index])
-//dynamically append btns
-var btn1=document.createElement("button");
-console.log(game.choices[index][0])
-console.log(game.choices[index][1])
-console.log(game.choices[index][2])
-console.log(game.answer[index])
+function headerTitle() {
+    headerTitle.textContent =" Code Quiz";}
+    headerTitle()
 
-//when user onclick a btn
-//check the user's data against the current answer
-//if correct
-//go to the next card ()index++;
-//check to see if there are more cards if there are more card renderCard()
-//then call render card
+function headerTitle() {
+    headerTitle.textContent =" Code Quiz";
+  //clear the area blank canvas
+  //dynamically append the data to the page
+  console.log(game.question[index]);
+  //dynamically append btns
+  var btn1 = document.createElement("button");
+  console.log(game.choices[index][0]);
+  console.log(game.choices[index][1]);
+  console.log(game.choices[index][2]);
+  console.log(game.answer[index]);
 
+  //when user onclick a btn
+  //check the user's data against the current answer
+  //if correct
+  //go to the next card ()index++;
+  //check to see if there are more cards if there are more card renderCard()
+  //then call render card
 }
 
 function quizTimer() {
-    time = setInterval(function(){
-        timerLeft--;
-        
-    })
+  time = setInterval(function () {
+    timerLeft--;
+  });
 }
 
-$( "#start-quiz").click(function() {
-    renderCard();
-        //ask a series of questions
-        //timer begins
-        //wrong answ bring time
-        //any ans move to the next card
-        //if at end go to end game
-        //ask to add score
-        //ask for username and push score into ls
-
+$("#start-quiz").click(function () {
+  renderCard();
+  //ask a series of questions
+  //timer begins
+  //wrong answ bring time
+  //any ans move to the next card
+  //if at end go to end game
+  //ask to add score
+  //ask for username and push score into ls
 });
